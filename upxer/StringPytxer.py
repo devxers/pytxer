ea41a225e6279e46cd9fa23563b7d7cc124d69f1
GabrielDS <gabriel.santosti01@gmail.com>
2018-01-07 07:27:03 -0300

    rename capitalize_first_letter's name

--- upxer/StringPytxer.py
+++ upxer/StringPytxer.py
@@ -5,6 +5,6 @@ regex_first_letter_lowercase = re.compile(r"(^[^A-Z])")
 
 def capitalize(text):
     if bool(regex_first_letter_lowercase.search(text)):
-        return text[:1].upper() + text[1:]
+        return text[:1].upper() + text[1:].lower()
     else:
         return text
