b3117277f6568ff374157182b77e73402bedc293
GabrielDS <gabriel.santosti01@gmail.com>
2018-01-08 18:59:58 -0300

    create method abbreviate_name and capitalize_list_of_string

--- upxer/StringPytxer.py
+++ upxer/StringPytxer.py
@@ -2,6 +2,7 @@ import re
 
 regex_first_letter_lowercase = re.compile(r"(^[^A-Z])|\w([A-Z])")
 regex_capitalize_name = re.compile(r"\b[^\s^A-Z]")
+regex_abbreviate_name = re.compile(r"\s(\w{1})\w+")
 
 
 def is_uncapitalize(text):
@@ -19,6 +20,10 @@ def capitalize(text):
         return text
 
 
+def capitalize_list_of_strings(list_strings):
+    return [capitalize(word) for word in list_strings]
+
+
 def list_to_string(list):
     return " ".join(str(x) for x in list)
 
@@ -31,5 +36,18 @@ def capitalize_name(name):
             capitalize(word) for word in list_of_substrings]
 
         return list_to_string(list_of_substrings_capitalize)
+    else:
+        return name
+
+
+def abbreviate_name(name, max_len=12):
+    if len(name) > max_len:
+
+        string_abbreviate_name = regex_abbreviate_name.sub(r" \1.", name)
+
+        string_splitted = string_abbreviate_name.lower().split()
+
+        return list_to_string(capitalize_list_of_strings(string_splitted))
+
     else:
         return name
