d971421e57962f5f47a9e6af52c2950ce2b05672
GabrielDS <gabriel.santosti01@gmail.com>
2018-01-08 16:37:20 -0300

    fix problem in method capitalize_name

--- upxer/StringPytxer.py
+++ upxer/StringPytxer.py
@@ -3,11 +3,14 @@ import re
 regex_first_letter_lowercase = re.compile(r"(^[^A-Z])")
 regex_capitalize_name = re.compile(r"\b[^\s^A-Z]")
 
+
 def is_uncapitalize(text):
     return bool(regex_first_letter_lowercase.search(text))
 
+
 def is_uncapitalize_name(name):
     return bool(regex_capitalize_name.search(name))
+
 
 def capitalize(text):
     if is_uncapitalize(text):
@@ -15,18 +18,18 @@ def capitalize(text):
     else:
         return text
 
+
 def list_to_string(list):
     return " ".join(str(x) for x in list)
 
 
 def capitalize_name(name):
     if is_uncapitalize_name(name):
-        list_of_substrings = name.split()
+        list_of_substrings = name.lower().split()
 
-        list_of_substrings_capitalize = [capitalize(word) for word in list_of_substrings]
+        list_of_substrings_capitalize = [
+            capitalize(word) for word in list_of_substrings]
 
         return list_to_string(list_of_substrings_capitalize)
     else:
         return name
-
-
