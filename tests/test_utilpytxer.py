f95cad5c104f12802260bf92d1ea1a6c1ea8a7a3
GabrielDS <gabriel.santosti01@gmail.com>
2018-01-09 23:37:43 -0300

    create tests to function has_some_number

--- tests/test_utilpytxer.py
+++ tests/test_utilpytxer.py
@@ -1,7 +1,20 @@
 import handling_test
+from upxer import UtilPytxer
 import unittest
 
 class TestUtilPytxer(unittest.TestCase):
+
+def test_has_some_number(self):
+
+    string_with_number = "An0th3r 5tr1ng"
+
+    self.assertTrue(has_some_number(string_with_number))
+
+def another_test_has_some_number(self):
+
+    string_without_number = "Another String"
+
+    self.assertFalse(has_some_number(string_without_number))
 
 
 
