f771835fb1c58fef1cdcbf35ddc626e3acab5128
GabrielDS <gabriel.santosti01@gmail.com>
2018-01-10 06:56:32 -0300

    create method has_some_letter

--- upxer/UtilPytxer.py
+++ upxer/UtilPytxer.py
@@ -1,7 +1,10 @@
 import re
 
 regex_has_some_number = re.compile(r"(\d)")
+regex_has_some_letter = re.compile(r"([A-z])")
 
 def has_some_number(text):
     return bool(regex_has_some_number.search(text))
 
+def has_some_letter(text):
+    return bool(regex_has_some_letter.search(text))
