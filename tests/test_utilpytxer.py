9a9417be333ff5d3c8fc9e1e211be270fbbbd37d
GabrielDS <gabriel.santosti01@gmail.com>
2018-01-09 23:42:56 -0300

    create tests to function has_some_letter

--- tests/test_utilpytxer.py
+++ tests/test_utilpytxer.py
@@ -16,6 +16,23 @@ def another_test_has_some_number(self):
 
     self.assertFalse(has_some_number(string_without_number))
 
+def test_has_some_letter(self):
+
+    string_with_letters = "5884899A88B"
+
+    self.assertTrue(has_some_letter(string_with_letters))
+
+def another_test_has_some_letter(self):
+
+    string_without_letters = "854494187"
+
+    self.assertFalse(has_some_letter(string_without_letters))
+
+def test_has_some_letter_whit_expected_true(self):
+
+    string_with_letters = "4A 6A A3 E8 A5S"
+
+    self.assertTrue(has_some_letter(string_with_letters))
 
 
 if __name__ == '__main__':
